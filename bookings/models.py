1f6468b6a25a54c0a7f7d330ea4c56275103f38d
e63g <c90gtx@icloud.com>
2022-01-03 09:22:58 +0100

    models done

--- bookings/models.py
+++ bookings/models.py
@@ -5,3 +5,23 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+class Customer(models.Model):
+    email = models.EmailField()
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, blank=True, related_name="cuser"
+    )
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    length = models.PositiveIntegerField()
+
+
+class Appointment(models.Model):
+    customer = models.ForeignKey("User", on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    service = models.ManyToManyField(Service, related_name="service")
